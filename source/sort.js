8fcdd6474f187d24c7323840e3c0c9a6554d4489
AlexisOMG <alesha-gramazeka@yandex.ru>
2021-02-08 22:27:30 +0300

    Added realization of sort

---
+++ source/sort.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+const compare = (a, b) => a.localeCompare(b);
+
+const sortSingleItem = (item) => item.split('').sort(compare).join('');
+
+const sort = (str) => str.toLowerCase()
+  .split(/\s+/)
+  .map(sortSingleItem)
+  .sort(compare)
+  .map(capitalize)
+  .join(' ');
